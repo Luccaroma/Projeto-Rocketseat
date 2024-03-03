f74975f16904b0597d8285e1df14149c795bbfbe
Lucca Rocha Martins <98410429+Luccaroma@users.noreply.github.com>
2024-03-03 13:54:20 -0300

    initial commit

---
+++ projeto Lucca/index.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    //html.classList, toggle('ligth')
+
+    if (html.classList.contains('ligth')) {
+    html.classList.remove('ligth')
+    } else {
+      html.classList.add('ligth')
+    }
+
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains('ligth')) {
+        img.setAttribute('src', "./assets/luccafoto-ligth.png")
+    } else {
+        img.setAttribute('src', "./assets/luccafoto.png")
+    }
+}
